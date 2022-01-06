af54a59abec15a65a8b0f6ecf316c5dd6e6ac48b
Sudhanshu Dekate <77233279+xidddekate@users.noreply.github.com>
2022-01-06 08:25:04 +0530

    Time: 6 ms (29.80%), Space: 41.6 MB (17.51%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -2,33 +2,19 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> tmp = new ArrayList<>();
-        Set<List<Integer>> s = new HashSet<>();
-        solve(ans,0,0,target,candidates,tmp,s);
+        solve(ans,tmp,candidates,target,0);
         return ans;
     }
-    void solve(List<List<Integer>> ans, int currInd, int currSum, int n, int[] arr, List<Integer> tmp,Set<List<Integer>> s){
-        if(currSum==n && !s.contains(new ArrayList<>(tmp))){
+    void solve(List<List<Integer>> ans, List<Integer> tmp, int[] arr, int n, int start){
+        if(n<0)return;
+        if(n==0){
             ans.add(new ArrayList<>(tmp));
-            // System.out.println(tmp);
-            s.add(tmp);
             return;
         }
-        if(currInd>=arr.length){
-            return;
-        }
-        
-        if(currSum + arr[currInd] <= n){
-            tmp.add(arr[currInd]);
-            // System.out.println("Forst "+currSum+arr[currInd]);
-            solve(ans,currInd,currSum+arr[currInd],n,arr,tmp,s);
+        for(int i=start;i<arr.length;i++){
+            tmp.add(arr[i]);
+            solve(ans,tmp,arr,n-arr[i],i);
             tmp.remove(tmp.size()-1);
         }
-        if(currInd+1 < arr.length && currSum+arr[currInd+1]<=n){
-            tmp.add(arr[currInd+1]);
-            // System.out.println("Second "+currSum+arr[currInd+1]);
-            solve(ans,currInd+1,currSum+arr[currInd+1],n,arr,tmp,s);
-            tmp.remove(tmp.size()-1);
-        }
-        solve(ans,currInd+1,currSum,n,arr,tmp,s);
     }
 }
